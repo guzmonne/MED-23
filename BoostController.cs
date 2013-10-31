629388ec3aafff6c638123606a3352978c04cfdd
Guzman Monne <guzmonne@hotmail.com>
2013-10-31 11:36:48 +0200

    Changes to the shooting animations

--- BoostController.cs
+++ BoostController.cs
@@ -5,19 +5,33 @@ public class BoostController : MonoBehaviour {
 	public Transform movingParticles;
 	public Transform staticParticles;
 	
+	private float y = 0;
+	private float z = 0;
+	private float tita = 0;
 	private PlayerControl playerControl;
+	private float currentSpeed;
+	private Vector3 velocity;
 	
 	void Awake() {
 		playerControl = GetComponent<PlayerControl>();
 	}
 	
-	
 	void Update() {
+		velocity = transform.TransformDirection(rigidbody.velocity);
+		y = velocity.y;
+		z = velocity.z;
+		tita = (z != 0) ? (Mathf.Atan(y/z) * Mathf.Rad2Deg) : tita;
+		currentSpeed = playerControl.currentSpeed;
 		if(Input.GetButton("Boost")){
 			staticParticles.gameObject.SetActive(true);
-			if(playerControl.currentSpeed != 0){
-				Debug.Log("Current Speed is:" + playerControl.currentSpeed.ToString() + ". So, set the throters!");
+			if (currentSpeed != 0){
 				movingParticles.gameObject.SetActive(true);
+				if (currentSpeed > 0)
+					movingParticles.transform.eulerAngles = new Vector3( 180 - tita, 0, 0);
+				if (currentSpeed < 0)
+					movingParticles.transform.eulerAngles = new Vector3( tita, 0, 0);
+			} else {
+				movingParticles.gameObject.SetActive(false);
 			}
 		} else {
 			staticParticles.gameObject.SetActive(false);
--- Scripts/PlayerControl.cs
+++ Scripts/PlayerControl.cs
@@ -7,13 +7,12 @@ public class PlayerControl : MonoBehaviour {
 	public float maxVelocityChange = 10.0f;
 	public float gravity = 30.0f;
 	public float jumpHeight = 4.0f;
-	public float distanceToWall;
-	public float distanceToGround;
+	public float boostValue = 1.5f;
 	// Public Hidden Variables
 	[HideInInspector]
 	public float currentSpeed;
 	// Private Character Properties
-	//public bool grounded = false;
+	private bool grounded = false;
 	private RaycastHit hit;
 	private Vector3 velocity;
 	private Vector3 c;
@@ -27,6 +26,10 @@ public class PlayerControl : MonoBehaviour {
 	private float skin = 0.07f;
 	private float xAxis;
 	private float direction;
+	private float boostMultiplier;
+	private float distanceToWall;
+	private float distanceToGround;
+	private int framesTillAction = 20;
 	private bool firstSlideFrame = true;
 	// Components
 	private Animator animator;
@@ -36,7 +39,8 @@ public class PlayerControl : MonoBehaviour {
 		Running,
 		Jumping,
 		Sliding,
-		Falling
+		Falling,
+		Shooting
 	}
 	public State playerState;
 	
@@ -56,29 +60,44 @@ public class PlayerControl : MonoBehaviour {
 		// Set the Character to the Falling state
 		playerState = State.Falling;
 	}
-	
+
 	void FixedUpdate() {
+		// Check if the character is Grounded
+		Grounded();
+		// Set the boost multiplier if the character is pressing "Boost"
+		boostMultiplier = (Input.GetButton("Boost")) ? boostValue : 1f;
 		// Grab the Player Horizontal movement
 		xAxis = Input.GetAxis("Horizontal");
+		// Test for Player and Character events depending on the Character State
 		switch (playerState){
 		case State.Standing:
 			Debug.Log("Standing");
+			// Set the Idle animation
+			currentSpeed = 0;
+			animator.SetFloat("Speed", Mathf.Abs(currentSpeed));
 			// Check if Player is making the character run
 			if( xAxis != 0){
 				playerState = State.Running;
 				break;
 			}
-			// Check if Player has jumped
-			if(Input.GetButton("Jump")){
+			// Check if the Player has jumped
+			if(Input.GetButton("Jump") && framesTillAction > 20){
 				Jump();
 				break;
 			}
-			// Set the Idle animation
-			currentSpeed = 0;
-			animator.SetFloat("Speed", Mathf.Abs(currentSpeed));
+			// Check if the player is shooting
+			if(Input.GetButton("Fire")){
+				playerState = State.Shooting;
+				break;
+			}
 			break;
 		case State.Running:
 			Debug.Log("Running");
+			// Check if the Character it's not on the ground
+			if(!grounded){
+				playerState = State.Falling;
+				break;
+			}
 			// Move the character horizontally
 			CalculateHorizontalForce();
 			// Set the Running animation
@@ -91,17 +110,17 @@ public class PlayerControl : MonoBehaviour {
 				break;
 			}
 			// Check if Player has jumped
-			if(Input.GetButton("Jump")){
+			if(Input.GetButton("Jump") && framesTillAction > 10){
 				Jump();
 				break;
 			}
-			if(Input.GetButton("Slide") && Mathf.Abs(currentSpeed) > 5){
+			if(Input.GetButton("Slide") && framesTillAction > 10){
 				Slide();
 				break;
 			}
-			// Check if the Character it's not on the ground
-			if(!Grounded()){
-				playerState = State.Falling;
+			// Check if the player is shooting
+			if(Input.GetButton("Fire")){
+				playerState = State.Shooting;
 				break;
 			}
 			break;
@@ -114,45 +133,57 @@ public class PlayerControl : MonoBehaviour {
 			if (xAxis != 0)
 				TurnAround();
 			// Check if the Character has landed
-			if(Grounded()){
+			if(grounded){
 				Stand();
 				break;
 			}
 			break;
 		case State.Sliding:
 			Debug.Log("Sliding");
-			if(firstSlideFrame){
-				rigidbody.AddForce(Vector3.forward * Mathf.Sign(direction) * 10, ForceMode.Impulse);
-				firstSlideFrame = false;
-			}
-			if(TouchingVertical("any", "up", 1f)){
-				CalculateHorizontalForce(true);
-				Debug.Log("Me toca el bocho la concha de la lora!!!");
-			} else {
-				if(Mathf.Abs(rigidbody.velocity.z) < 3){
-					firstSlideFrame = true; 
-					Stand();
-					break;
-				}
-			}
-			/*if(!Grounded()){
+			CalculateSlideForce();
+			break;
+		case State.Shooting:
+			Debug.Log("Shooting");
+			// Check if the Character it's not on the ground
+			if(!grounded){
 				playerState = State.Falling;
 				break;
-			}*/
-			/*if(!TouchingVertical("any", "up")){
+			}
+			//	Set the Shooting animation
+			animator.SetBool("isShooting", true);
+			animator.SetFloat("Speed", Mathf.Abs(currentSpeed));
+			CalculateHorizontalForce();
+			// Check if the Player has changed the direction of the jump
+			if (xAxis != 0)
+				TurnAround();
+			// Check if Player has jumped
+			if(Input.GetButton("Jump") && framesTillAction > 10){
+				// Stop the shooting animation
+				animator.SetBool("isShooting", false);
+				Jump();
+				break;
+			}
+			if(Input.GetButton("Slide") && framesTillAction > 10){
+				animator.SetBool("isShooting", false);
+				Slide();
+				break;
+			}
+			// Check if the Player has stopped shooting
+			if(!Input.GetButton("Fire")){
 				Stand();
 				break;
-			}*/
+			}
 			break;
 		case State.Falling:
 			Debug.Log("Falling");
 			CalculateHorizontalForce();
 			// Set the Falling animation
 			animator.SetBool("isFalling", true);
+			// Check if the Character has turned around
 			if (xAxis != 0)
 				TurnAround();
 			// Check if the Character has landed
-			if(Grounded()){
+			if(grounded){
 				Stand();
 				break;
 			}
@@ -160,6 +191,13 @@ public class PlayerControl : MonoBehaviour {
 		}
 		// We apply gravity manually for more tuning control
 	    rigidbody.AddForce(new Vector3 (0, -gravity * rigidbody.mass, 0));
+		// We count the frames till the next jump and we reset it if the character is not grounded
+		if(framesTillAction < 50){
+			framesTillAction++;
+		}
+		if(!grounded || Input.GetButton("Slide")){
+			framesTillAction = 0;
+		}
 	}
 //***********************************************************************************************//
 //										MAIN FUNCTIONS
@@ -176,7 +214,7 @@ public class PlayerControl : MonoBehaviour {
 	        Vector3 targetVelocity = new Vector3(0, 0, zTargetVelocity);
 	        // Transforms direction from local space to world space. Then multiply by speed
 			targetVelocity = transform.TransformDirection(targetVelocity);
-	        targetVelocity *= speed;
+	        targetVelocity *= (speed * boostMultiplier);
 			// We grab the current rigidbody velocity
 	        velocity = rigidbody.velocity;
 			// We save the velocity in z to the currentSpeed variable
@@ -196,6 +234,27 @@ public class PlayerControl : MonoBehaviour {
 	/// </summary>
 	private void CalculateVerticalForce(){
 		rigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+	}
+	/// <summary>
+	/// Calculates the slide force.
+	/// </summary>
+	private void CalculateSlideForce() {
+		// Give a little impulse when sliding only if it is the first frame of the slide
+		if(firstSlideFrame){
+			rigidbody.AddForce(Vector3.forward * Mathf.Sign(direction) * 10 * boostMultiplier, ForceMode.Impulse);
+			firstSlideFrame = false;
+		}
+		// If the character is sliding under anything then the movement continues until he is out
+		// Mainly to solve the problem of getting stuck under a low roof that you need to slide under
+		// Also, we stand the character immediately after there is no more vertical contraints.
+		if(TouchingVertical("any", "up", 1f)){
+			CalculateHorizontalForce(true);
+		} else {
+			if(Mathf.Abs(rigidbody.velocity.z) < 3){
+				firstSlideFrame = true; 
+				Stand();
+			}
+		}
 	}
 	/// <summary>
 	/// Characters the hits wall.
@@ -232,7 +291,7 @@ public class PlayerControl : MonoBehaviour {
 	private float CalculateJumpVerticalSpeed () {
 	    // From the jump height and gravity we deduce the upwards speed 
 	    // for the character to reach at the apex.
-	    return Mathf.Sqrt(2 * jumpHeight * gravity);
+	    return Mathf.Sqrt(2 * jumpHeight * gravity * boostMultiplier);
 	}
 	/// <summary>
 	/// Jump steps.
@@ -247,11 +306,12 @@ public class PlayerControl : MonoBehaviour {
 	/// </summary>
 	private void Stand() {
 		playerState = State.Standing;
-		ResetCollider();
 		// Set the Standing animation
 		animator.SetBool("isFalling", false);
 		animator.SetBool("isJumping", false);
 		animator.SetBool("isSliding", false);
+		animator.SetBool("isShooting", false);
+		ResetCollider();
 	}
 	private void Slide() {
 		playerState = State.Sliding;
@@ -292,15 +352,19 @@ public class PlayerControl : MonoBehaviour {
 	/// Resets the collider.
 	/// </summary>
 	private void ResetCollider(){
+		// We wait for 20 frames to pass before we reset the collider
 		SetCollider(originalHeight, originalRadius, originalCentre);
 	}
 //***********************************************************************************************//
 //									  COLLISIONS FUNCTIONS
 //***********************************************************************************************//	
+	
 	/// <summary>
 	/// Raises the collision stay event.
 	/// </summary>
-	
+	/// <param name='collisionInfo'>
+	/// Collision info.
+	/// </param>
 	void OnCollisionStay(Collision collisionInfo){
 		switch (collisionInfo.gameObject.name) {
 		case "MovingPlatform":
@@ -308,7 +372,12 @@ public class PlayerControl : MonoBehaviour {
 			break;
 		} 
 	}
-	
+	/// <summary>
+	/// Raises the collision enter event.
+	/// </summary>
+	/// <param name='collisionInfo'>
+	/// Collision info.
+	/// </param>
 	void OnCollisionEnter(Collision collisionInfo){
 		switch (collisionInfo.gameObject.name) {
 		case "MovingPlatform":
@@ -316,7 +385,12 @@ public class PlayerControl : MonoBehaviour {
 			break;
 		}
 	}
-	
+	/// <summary>
+	/// Raises the collision exit event.
+	/// </summary>
+	/// <param name='collisionInfo'>
+	/// Collision info.
+	/// </param>
 	void OnCollisionExit(Collision collisionInfo){
 		switch (collisionInfo.gameObject.name) {
 		case "MovingPlatform":;
@@ -324,8 +398,16 @@ public class PlayerControl : MonoBehaviour {
 			break;
 		} 
 	}
-	
-	private bool TouchingSide(string platform) {
+	/// <summary>
+	/// Checks if the character touches something in front.
+	/// </summary>
+	/// <returns>
+	/// True if he touched something
+	/// </returns>
+	/// <param name='platform'>
+	/// If set to <c>true</c> platform.
+	/// </param>
+	private bool TouchingHorizontal(string platform) {
 		for(int i = 0; i < 3; i++){
 			Vector3 origin = transform.position + new Vector3(0, h/4 * (1+i), 0);
 			if (Physics.Raycast(origin, transform.TransformDirection(Vector3.forward), out hit, skin)){
@@ -335,7 +417,21 @@ public class PlayerControl : MonoBehaviour {
 		}
 		return false;
 	}
-	
+	/// <summary>
+	/// Checks if the character touches something on top or below him.
+	/// </summary>
+	/// <returns>
+	/// True if he touched something
+	/// </returns>
+	/// <param name='platform'>
+	/// If set to <c>true</c> platform.
+	/// </param>
+	/// <param name='upOrDown'>
+	/// If set to <c>true</c> up or down.
+	/// </param>
+	/// <param name='raySize'>
+	/// If set to <c>true</c> ray size.
+	/// </param>
 	private bool TouchingVertical(string platform, string upOrDown, float raySize = 0) {
 		float dir = (upOrDown == "up") ? 1 : -1;
 		for (int i = 0; i < 2; i++){
@@ -347,17 +443,22 @@ public class PlayerControl : MonoBehaviour {
 		}
 		return false;
 	}
-	
-	private bool Grounded(){
-		for (int i = 0; i < 3; i++){
-			Debug.DrawRay(transform.position + new Vector3(0, skin * colliderScale, -direction * (i - 1) * r), -transform.TransformDirection(Vector3.up), Color.red);	
-			if (Physics.Raycast(transform.position + new Vector3(0, skin * colliderScale,  (i - 1) * r ) , -transform.TransformDirection(Vector3.up), out hit, 1)){
+	/// <summary>
+	/// Check if the character is Grounded
+	/// </summary>
+	private void Grounded(){
+		bool setGrounded = false;
+		for (float i = 0; i < 7; i++){
+			Debug.DrawRay(transform.position + new Vector3(0, skin * colliderScale, (i/3 - 1) * r), -transform.TransformDirection(Vector3.up), Color.blue);	
+			if (Physics.Raycast(transform.position + new Vector3(0, skin * colliderScale,  (i/3 - 1) * r ) , -transform.TransformDirection(Vector3.up), out hit, 1F)){
             	distanceToGround = hit.distance;
 				if(distanceToGround < skin){
-					return true;
+					//grounded = true;
+					setGrounded = true;
+					break;
 				}
     		} 	
 		}
-		return false;
+		grounded = (setGrounded) ? true : false;
 	}
 }
